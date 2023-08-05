b3989238a2094de82ba19797c2f5534479e12d9a
程序猿DD <dyc87112@qq.com>
2023-08-05 22:47:00 +0800

    init

---
+++ src/main/java/com/spring4all/minipay/MiniPayApplication.java
@@ -0,0 +1,13 @@
+package com.spring4all.minipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring4all/minipay/MiniPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring4all.minipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
